ac504187896fb45c3ff4f39c8713689401dd30af
adrag239 <adragunov@gmail.com>
2018-06-10 18:16:44 +0200

    Added basic master-detail pages

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import { Router, RouterConfiguration } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Systembolaget';
+    config.map([
+      { route: '', moduleId: PLATFORM.moduleName('list'), title: 'Articles' },
+      { route: 'detail/:id', moduleId: PLATFORM.moduleName('detail'), name: 'detail' }
+    ]);
+
+    this.router = router;
+  }
 }
